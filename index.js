0f6258960849e9dca8f560ab6ce1059074eba559
Kunal <kunalukey32@gmail.com>
2024-01-19 01:16:09 +0530

    added mongoose for database

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // middleware
 app.use(express.json());
@@ -11,7 +13,19 @@ app.get('/', (req, res) => {
     })
 });
 
-// Listen to PORT
-app.listen(process.env.PORT || 5000, () => {
-    console.log('listening on port' + " " + process.env.PORT || 5000);
+app.get('/blogs', async (req, res) => {
+    const blogs = await Blog.find({});
+
+    res.send({
+        blogs: blogs
+    })
 })
+
+// Listen to PORT
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+        console.log('listening on port' + " " + process.env.PORT || 5000);
+    })
+}).catch(error => {
+    console.log(error)
+});
